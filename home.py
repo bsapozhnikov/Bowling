2d0a277bedc44f7998e811f4ae59484c3d348ea4
Brian Sapozhnikov <brian.sapozhnikov@gmail.com>
2014-10-15 21:01:50 -0400

    changed score calculator, still need to account for multiple consecutive strikes

--- home.py
+++ home.py
@@ -16,17 +16,28 @@ for player in people:
     L = player.strip('\n').split('|')
     name = L[0]
     print name
-    game_totals = [1 for x in xrange(21)]
+    ball_totals = [0 for x in xrange(21)]
     balls = L[1].split(' ')
     print balls
     i = 0
-    while i<18:
+    while i<20:
+        if i>1:
+            if int(balls[i-2])==10:
+                ball_totals[i-1]+=int(balls[i])+int(balls[i+1])
+            elif int(balls[i-2])+int(balls[i-1])==10:
+                ball_totals[i-1]+=int(balls[i])
+        
         if i>0:
-            game_totals[i]=game_totals[i]*int(balls[i])+game_totals[i-1]
+            ball_totals[i]=int(balls[i])+ball_totals[i-1]
         else:
-            game_totals[i]=int(balls[i])+int(balls[i+1])
-        game_totals[i+1]=game_totals[i+1]*int(balls[i+1])+game_totals[i]
-        print `i/2`+' '+balls[i]+' '+balls[i+1]+' '+`game_totals[i+1]`        
+            ball_totals[i]=int(balls[i])
+       
+        ball_totals[i+1]=int(balls[i+1])+ball_totals[i]
         i+=2
+    ball_totals[20]=ball_totals[19]+int(balls[20])
+    print ball_totals
+    frame_totals = [ball_totals[i*2+1] for i in xrange(10)]
+    frame_totals[-1]+=int(balls[20])
+    print frame_totals
 
 ##return page
