bb9c08e340964aa6b3db11071e11dedb6855ff76
Brian Sapozhnikov <brian.sapozhnikov@gmail.com>
2014-10-19 15:58:20 -0400

    line endings fixed, multiple games per person

--- home.py
+++ home.py
@@ -20,35 +20,41 @@ for player in people:
     name = L[0]
     ##print name
     ball_totals = [0 for x in xrange(21)]
-    balls = L[1].split(' ')
-    ##print balls
-    i = 0
-    while i<20:
-        if i>1:
-            if int(balls[i-2])==10:
-                if int(balls[i-4])==10:
-                    ball_totals[i-3]+=int(balls[i])
+    games = []
+    player_totals = []
+    for game in L[1:]:
+        balls = game.split(' ')
+        ##print balls
+        i = 0
+        while i<20:
+            if i>1:
+                if int(balls[i-2])==10:
+                    if int(balls[i-4])==10:
+                        ball_totals[i-3]+=int(balls[i])
+                        ball_totals[i-1]+=int(balls[i])
+                    ball_totals[i-1]+=int(balls[i])+int(balls[i+1])
+                elif int(balls[i-2])+int(balls[i-1])==10:
                     ball_totals[i-1]+=int(balls[i])
-                ball_totals[i-1]+=int(balls[i])+int(balls[i+1])
-            elif int(balls[i-2])+int(balls[i-1])==10:
-                ball_totals[i-1]+=int(balls[i])
-        
-        if i>0:
-            ball_totals[i]=int(balls[i])+ball_totals[i-1]
-        else:
-            ball_totals[i]=int(balls[i])
-       
-        ball_totals[i+1]=int(balls[i+1])+ball_totals[i]
-        i+=2
-    ball_totals[20]=ball_totals[19]+int(balls[20])
-    ##print ball_totals
-    frame_totals = [ball_totals[i*2+1] for i in xrange(10)]
-    frame_totals[-1]+=int(balls[20])
-    ##print frame_totals
+                        
+            if i>0:
+                ball_totals[i]=int(balls[i])+ball_totals[i-1]
+            else:
+                ball_totals[i]=int(balls[i])
+                
+            ball_totals[i+1]=int(balls[i+1])+ball_totals[i]
+            i+=2
+        ball_totals[20]=ball_totals[19]+int(balls[20])
+        ##print ball_totals
+        frame_totals = [ball_totals[i*2+1] for i in xrange(10)]
+        frame_totals[-1]+=int(balls[20])
+        ##print frame_totals
+        games.append(balls)
+        player_totals.append(frame_totals)
+
     tabledata+='<tr><td>'+name+'''<input type="button" value="button" onclick="toggle(\''''+name+'''\')"></input></td>'''
     tabledata+='<td></td><td></td><td></td><td></td><td></td></tr>'
-    js+='var '+name+'scores = '+`balls`+';\n'
-    js+='var '+name+'totals = '+`frame_totals`+';\n'
+    js+='var '+name+'scores = '+`games`+';\n'
+    js+='var '+name+'totals = '+`player_totals`+';\n'
 
 ##print tabledata
 ##print js    
