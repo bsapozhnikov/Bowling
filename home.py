cf762b809274ddf7671675b01ddc6e80866a5c1c
Brian Sapozhnikov <brian.sapozhnikov@gmail.com>
2014-10-20 20:08:28 -0400

    clickable row debugging

--- home.py
+++ home.py
@@ -78,10 +78,10 @@ for player in people:
         player_totals.append(frame_totals)
         total_pins+=frame_totals[-1]
 
-    tabledata+='<a value="button" onclick="toggle(\''''+name+'''\')"><tr><td>'''+name+'''</td>'''
-    tabledata+='<td>'+`len(games)`+'</td>'
-    tabledata+='<td>'+`total_pins`+'</td>'
-    tabledata+='<td>'+`1.0*total_pins/len(games)`+'</td></tr></a>'
+    tabledata+='<tr><td><a value="button" onclick="toggle(\''''+name+'''\')">'''+name+'''</a></td>'''
+    tabledata+='<td><a value="button" onclick="toggle(\''+name+'\')">'+`len(games)`+'</a></td>'
+    tabledata+='<td><a value="button" onclick="toggle(\''+name+'\')">'+`total_pins`+'</a></td>'
+    tabledata+='<td><a value="button" onclick="toggle(\''+name+'\')">'+`1.0*total_pins/len(games)`+'</a></td></tr>'
     js+='var '+name+'scores = '+`games`+';\n'
     js+='var '+name+'totals = '+`player_totals`+';\n'
 
